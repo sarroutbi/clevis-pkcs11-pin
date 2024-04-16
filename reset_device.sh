73efff5ee2d64ebc56034a36b76ea17744785d4e
Sergio Arroutbi <sarroutb@redhat.com>
2024-04-16 09:58:55 +0200

    Include default options through here documents (#2)
    Resolves: #1
    
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- reset_device.sh
+++ reset_device.sh
@@ -4,8 +4,13 @@ PUBKEY_FILE=pubkey.pem
 SUBJECT=clevis
 
 "${YUBICO_HANDLER}" piv reset --force
-"${YUBICO_HANDLER}" piv keys generate -a RSA2048 9d "${PUBKEY_FILE}"
-"${YUBICO_HANDLER}" piv certificates generate --subject "${SUBJECT}" 9d "${PUBKEY_FILE}"
+"${YUBICO_HANDLER}" piv keys generate -a RSA2048 9d "${PUBKEY_FILE}"<<EOF
+
+EOF
+"${YUBICO_HANDLER}" piv certificates generate --subject "${SUBJECT}" 9d "${PUBKEY_FILE}"<<EOF
+
+123456
+EOF
 "${YUBICO_HANDLER}" piv info
 
 rm -v "${PUBKEY_FILE}"
