9ec95030d9b9ef13b66224780f44afd59437020a
Sergio Arroutbi <sarroutb@redhat.com>
2024-06-13 09:22:42 +0200

    Parameterize file, key and iterations (#44)
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- clevis-afunix-socket-unlock.c
+++ clevis-afunix-socket-unlock.c
@@ -27,70 +27,100 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-const char* DATE = "202406111314";
+const char* VERSION = "202406120948";
+const uint16_t MAX_ITERATIONS = 3;
+const uint16_t MAX_KEY = 256;
+const uint16_t MAX_PATH = 1024;
 
-int main(int argc, char* arvg[]) {
-	int s, a;
-	int ret;
-	struct sockaddr_un control_addr, acept_addr, peer_addr, anon_addr;
-	socklen_t len;
-	socklen_t pathlen;
+int main(int argc, char* argv[]) {
+    int s, a, opt, ret;
+    uint32_t iterations = MAX_ITERATIONS;
+    uint32_t ic = 0;
+    char file[MAX_PATH];
+    char key[MAX_KEY];
+    struct sockaddr_un control_addr, acept_addr, peer_addr, anon_addr;
+    socklen_t len;
+    socklen_t pathlen;
 
-	// TODO: Make this configurable in program input
-	const char* path = "/run/systemd/clevis-pkcs11.sock";
-	memset(&control_addr, 0, sizeof(control_addr));
-	control_addr.sun_family = AF_UNIX;
-	strcpy(control_addr.sun_path, path);
-	unlink(path);
-
-	s = socket(AF_UNIX, SOCK_STREAM, 0);
-	if (s == -1) {
-            perror("socket");
-            exit(EXIT_FAILURE);
-	}
-
-	ret = bind(s, (struct sockaddr *)&control_addr, sizeof(control_addr));
-	if (ret == -1) {
-            perror("bind");
-            exit(EXIT_FAILURE);
+    while ((opt = getopt(argc, argv, "f:k:i:")) != -1) {
+        switch (opt) {
+        case 'f':
+          strncpy(file, optarg, MAX_PATH);
+          break;
+        case 'k':
+          strncpy(key, optarg, MAX_KEY);
+          break;
+        case 'i':
+          iterations = strtoul(optarg, 0, 10);
+          break;
+        default: /* '?' */
+          fprintf(stderr, "Usage: %s -f file -k key [-i iterations, 3 by default]\n",
+                  argv[0]);
+          exit(EXIT_FAILURE);
         }
+    }
+    printf("VERSION: [%s]\n", VERSION);
+    printf("FILE: [%s]\n", file);
+    printf("KEY: [%s]\n", key);
+    printf("ITERATIONS: [%u]\n", iterations);
 
-	ret = listen(s, SOMAXCONN);
-	if (ret == -1) {
-            perror("listen");
-            exit(EXIT_FAILURE);
-        }
+    memset(&control_addr, 0, sizeof(control_addr));
+    control_addr.sun_family = AF_UNIX;
+    strcpy(control_addr.sun_path, file);
+    unlink(file);
 
-	len = sizeof(acept_addr);
-	a = accept(s, (struct sockaddr *)&acept_addr, &len);
-	if (a == -1) {
+    s = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (s == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = bind(s, (struct sockaddr *)&control_addr, sizeof(control_addr));
+    if (ret == -1) {
+        perror("bind");
+        exit(EXIT_FAILURE);
+    }
+
+    ret = listen(s, SOMAXCONN);
+    if (ret == -1) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    len = sizeof(acept_addr);
+
+    while (ic++ < iterations++) {
+        a = accept(s, (struct sockaddr *)&acept_addr, &len);
+        if (a == -1) {
             perror("accept");
             exit(EXIT_FAILURE);
         }
 
-	pathlen = len - offsetof(struct sockaddr_un, sun_path);
+        pathlen = len - offsetof(struct sockaddr_un, sun_path);
 
-	len = sizeof(peer_addr);
-	ret = getpeername(a, (struct sockaddr *)&peer_addr, &len);
-	if (ret == -1) {
+        len = sizeof(peer_addr);
+        ret = getpeername(a, (struct sockaddr *)&peer_addr, &len);
+        if (ret == -1) {
             perror("getpeername");
             exit(EXIT_FAILURE);
         }
 
-	pathlen = len - offsetof(struct sockaddr_un, sun_path);
-	char peer[pathlen];
-	memset(peer, 0, pathlen);
-	strncpy(peer, peer_addr.sun_path+1, pathlen-1);
-	printf("VERSION: [%s]\n", DATE);
-	printf("getpeername sun_path(peer): [%s]\n", peer);
+        pathlen = len - offsetof(struct sockaddr_un, sun_path);
+        char peer[pathlen];
+        memset(peer, 0, pathlen);
+        strncpy(peer, peer_addr.sun_path+1, pathlen-1);
+        printf("Pending tries: [%u/%u]\n", iterations, MAX_ITERATIONS);
+        printf("getpeername sun_path(peer): [%s]\n", peer);
 
-	// Now we have all the information in peer, something like:
-	// \099226072855ae2d8/cryptsetup/luks-6e38d5e1-7f83-43cc-819a-7416bcbf9f84
-	// NUL random /cryptsetup/ DEVICE
-	// If we need to unencrypt device, pick it from peer information
-	// To return the key, just respond to socket returned by accept
-	// TODO: Make this configurable in program input
-	const char* key = "YOUR_KEY_HERE";
-	send(a, key, strlen(key), 0);
-	return EXIT_SUCCESS;
+        // Now we have all the information in peer, something like:
+        // \099226072855ae2d8/cryptsetup/luks-6e38d5e1-7f83-43cc-819a-7416bcbf9f84
+        // NUL random /cryptsetup/ DEVICE
+        // If we need to unencrypt device, pick it from peer information
+        // To return the key, just respond to socket returned by accept
+        // TODO: Make this configurable in program input
+        const char* key = "wd1uvGgpB_WvDJksxjiug5MnZYBVrB0uHzDswCtP6vu83-nAH6TLuw";
+        send(a, key, strlen(key), 0);
+        close(a);
+    }
+    return EXIT_SUCCESS;
 }
